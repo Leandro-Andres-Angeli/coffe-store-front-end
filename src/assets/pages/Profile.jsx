ceba3cdc3167fa39fca6d968218810514689f44d
= <=>
2024-11-09 20:10:06 -0300

    routing

--- src/assets/pages/Profile.jsx
+++ src/assets/pages/Profile.jsx
@@ -1,12 +1,35 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import {
+  Container,
+  Divider,
+  Grid,
+  GridColumn,
+  Menu,
+  MenuItem,
+} from 'semantic-ui-react';
 export const Account = () => <p>Account</p>;
 export const Favorites = () => <p>Favorites</p>;
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-      <Outlet></Outlet>
-    </div>
+    <Container>
+      <h1>Profile settings</h1>
+      <Divider></Divider>
+      <Grid>
+        <GridColumn stretched width={12}>
+          <Outlet></Outlet>
+        </GridColumn>
+        <GridColumn width={4}>
+          <Menu fluid vertical tabular='right'>
+            <MenuItem as={NavLink} to='account'>
+              Account{' '}
+            </MenuItem>
+            <MenuItem as={NavLink} to='favorites'>
+              Favorites{' '}
+            </MenuItem>
+          </Menu>
+        </GridColumn>
+      </Grid>
+    </Container>
   );
 };
 
